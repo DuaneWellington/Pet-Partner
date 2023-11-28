74c2a5167466991459d865d973d074242da41997
Duane Wellington <dwelling77@gmail.com>
2023-11-28 10:48:22 -0500

    finally arrive at functionality for new user form

--- controllers/users.js
+++ controllers/users.js
@@ -3,31 +3,62 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+// const users = require('../models/user');
 
-module.exports = {
-    newUser,
-    router
-  }
+//USER
+
+// function users (req, res) {
+//   res.render('animals/users', {title: "Users", errorMsg:''})
+// }
+
   
-  //handle POST requests to /users
-  router.post('/', async (req, res) => {
-    try {
-      // Get data from the form submission
-      const userData = req.body;
+//INDEX
 
-      // Create a new user instance with the form data
-      const newUser = new User(userData);
+async function index(req, res) {
+  try {
+      const allUsers = await User.find({});
+      console.log(allUsers, "allUsers");
 
-      //Save the new user to the database
-      await newUser.save();
+      res.render('animals/users', {title: "All Users", users: allUsers})
+  } catch (err) {
+      console.log('index error', err)
+  }
+}
 
-      //Render form
-      res.render('/aminals/users', {title: "New User", errorMsg: '' });
-    } catch (error) {
-      //Handle errors
-      res.render('error', { error});
-    }
-    });
+// CREATE
+async function create(req, res) {
+  try {
+    await User.create(req.body);
+    // Always redirect after CRUDing data
+    res.redirect('/users', {errorMsg: err.message});
+  } catch (err) {
+    // Typically some sort of validation error
+    console.log(err);
+    res.render('animals/users', { errorMsg: err.message });
+  }
+}
+
+
+  
+  // //handle POST requests to /users
+  // router.post('/', async (req, res) => {
+  //   try {
+  //     // Get data from the form submission
+  //     const userData = req.body;
+
+  //     // Create a new user instance with the form data
+  //     const newUser = new User(userData);
+
+  //     //Save the new user to the database
+  //     await newUser.save();
+
+  //     //Render form
+  //     res.render('/aminals/users', {title: "New User", errorMsg: '' });
+  //   } catch (error) {
+  //     //Handle errors
+  //     res.render('error', { error });
+  //   }
+  //   });
   
   //Add a new user
   async function newUser(req, res) {
@@ -36,6 +67,7 @@ module.exports = {
       const newUser = new User(req.body);
       //Save new user to the database
       await newUser.save();
+      // res.render('users/new', {title: "Add new User", users: await User.findById(req.params.id)})
 
       res.redirect(`/users/${newUser._id}`);
     } catch (err) {
@@ -43,3 +75,11 @@ module.exports = {
         res.render('error', { error: err });
     }
   }
+
+  module.exports = {
+    index,
+    create,
+    // users,
+      newUser,
+      router
+    }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router();
 
+//create the controller
 const usersCtrl = require('../controllers/users')
 
+//router -> GET /users using default path
+router.get('/', usersCtrl.index)
+
+//router -> POST /users
+// router.get('/users', usersCtrl.index)
+
+//router -> POST /users
 router.post('/', usersCtrl.newUser)
+
+
+
+
+// //router -> GET /pets/:id
+// router.get('/:id', usersCtrl.show)
+
+/* GET users page. */
+// router.get('/', function(req, res, next) {
+//     res.redirect('/users');
+//   });
+  
 
 module.exports = router
