4df995fcc24a26f6e9eb248eef32d49091e53f2f
sadiefiss <sadiefiss@gmail.com>
2023-11-30 17:44:01 -0500

    Oauth baseline

--- models/user.js
+++ models/user.js
@@ -1,103 +1,45 @@
 // PATH: models/user.js
 
-const mongoose = require("mongoose") //import mongoose
+const mongoose = require("mongoose"); //import mongoose
 
 const Schema = mongoose.Schema; //declare Schema
 
 const userSchema = new Schema(
   {
-    // add oauth info here
+    fullname: String,
+    googleId: {
+      type: String,
+      required: true,
+    },
+    email: String,
+    avatar: String,
     userInfo: {
-      firstName: {type: String},
-      lastName: {type: String},
-      emailAddress: {type: String},
+      firstName: { type: String },
+      lastName: { type: String },
+      emailAddress: { type: String },
       address: {
-        street1: {type: String},
-        street2: {type: String},
-        city: {type: String},
-        fosterState: {type: String},
-        postalCode: {type: String},
-        country: {type: String},
+        street1: { type: String },
+        street2: { type: String },
+        city: { type: String },
+        fosterState: { type: String },
+        postalCode: { type: String },
+        country: { type: String },
       },
-      phoneNo: { type: Number},
-      fosterStartDate: { type: Date},
-      fosterAge: { type: Number,
-        min: 1, 
-        max: 200
-      },
+      phoneNo: { type: Number },
+      fosterStartDate: { type: Date },
+      fosterAge: { type: Number, min: 1, max: 200 },
       fosterLength: {
-        twoWeeks: {type: Boolean},
-        oneMonth: {type: Boolean},
-        threeMonths: {type: Boolean},
-        sixMonths: {type: Boolean},
+        twoWeeks: { type: Boolean },
+        oneMonth: { type: Boolean },
+        threeMonths: { type: Boolean },
+        sixMonths: { type: Boolean },
       },
     },
   },
 
-
-    // userInfo: {
-    //   firstName: {
-    //     type: String,
-    //   },
-    //   lastName: {
-    //     type: String,
-    //   },
-    //   emailAddress: {
-    //     type: String,
-    //   },
-    //   address: {
-    //     street1: {
-    //       type: String,
-    //     },
-    //     street2: {
-    //       type: String,
-    //     },
-    //     city: {
-    //       type: String,
-    //     },
-    //     fosterState: {
-    //       type: String,
-    //     },
-    //     postalCode: {
-    //       type: String,
-    //     },
-    //     country: {
-    //       type: String,
-    //     },
-    //   },
-    //   fosterAge: {
-    //     type: Number,
-    //   },
-    //   fosterLength: {
-    //     twoWeeks: {
-    //       type: Boolean,
-    //     },
-    //     oneMonth: {
-    //       type: Boolean,
-    //     },
-    //     threeMonths: {
-    //       type: Boolean,
-    //     },
-    //     sixMonths: {
-    //       type: Boolean,
-    //     },
-    //   },
-    // },
-
-    // accessories: {
-    //     litterBox: Boolean,
-    //     leash: Boolean,
-    //     kennel: Boolean,
-    //     habitat: Boolean,
-    //     toys: Boolean },
-
-    // food: {
-    //     type: Boolean,
-    // },
-  
   {
-    timestamps: true
-  },
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,65 +1,68 @@
 //server.js
 
-const createError = require('http-errors');
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
+require("dotenv").config(); //added for .env
+require("./config/database"); // require the database
+require("./config/passport");
+const indexRouter = require("./routes/index");
+const petsRouter = require("./routes/pets");
+const usersRouter = require("./routes/users");
 
-require('dotenv').config(); //added for .env
-require('./config/database') // require the database
-
-const indexRouter = require('./routes/index');
-const petsRouter = require('./routes/pets');
-const usersRouter = require('./routes/users');
-
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/pets', petsRouter);
-app.use('/users', usersRouter);
-
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+app.use("/", indexRouter);
+app.use("/pets", petsRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
